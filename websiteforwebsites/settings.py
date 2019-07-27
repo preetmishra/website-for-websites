c81dbe5a6f075712e97dae0c1bf4773c61f9c06c
preetmishra <ipreetmishra@gmail.com>
2019-07-27 21:57:14 +0530

    Add django_heroku

--- websiteforwebsites/settings.py
+++ websiteforwebsites/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -163,3 +164,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'teamatwfw@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('WFW_EMAIL_PASSWORD')
+
+django_heroku.settings(locals())
