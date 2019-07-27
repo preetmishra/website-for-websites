5d12bfdb1acdae4f5fde6b6bcf0c002a7e83e767
preetmishra <ipreetmishra@gmail.com>
2019-07-27 20:35:03 +0530

    Update STATIC_ROOT

--- websiteforwebsites/settings.py
+++ websiteforwebsites/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_DIR = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
@@ -130,6 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = STATIC_DIR
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
