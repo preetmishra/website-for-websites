198dae9644e51ec29e211783c30faf0727099535
preetmishra <ipreetmishra@gmail.com>
2019-07-28 02:21:45 +0530

    Change email service

--- websiteforwebsites/settings.py
+++ websiteforwebsites/settings.py
@@ -158,10 +158,10 @@ LOGIN_URL = 'users:login'
 LOGOUT_REDIRECT_URL = 'catalog:index'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'teamatwfw@gmail.com'
+EMAIL_HOST_USER = 'apikey'
 EMAIL_HOST_PASSWORD = os.environ.get('WFW_EMAIL_PASSWORD')
 
 django_heroku.settings(locals())
