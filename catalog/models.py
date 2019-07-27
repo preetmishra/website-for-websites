c0572cb2eb48b2390fa9e176d3c2af0431a184c0
preetmishra <ipreetmishra@gmail.com>
2019-07-27 23:06:13 +0530

    Remove desc field

--- catalog/models.py
+++ catalog/models.py
@@ -18,7 +18,6 @@ class Website(models.Model):
 
     name = models.CharField(max_length=128, verbose_name='title', unique=True)
     url = models.URLField(help_text='Do not forget to add http:// or https://.')
-    description = models.CharField(max_length=256, blank=True, default='')
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='tags')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date_added = models.DateTimeField(default=timezone.now)
